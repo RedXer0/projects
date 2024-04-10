4ba85a5b3272b9586851f770ca352f054ebec113
Red Xer0 <165036246+RedXer0@users.noreply.github.com>
2024-04-10 12:38:47 -0500

    Update mist.cpp

--- MIST_1.0.1/mist.cpp
+++ MIST_1.0.1/mist.cpp
@@ -1,6 +1,8 @@
 /*
 Mist the terminal assistant.
 */
+
+// current libraries
 #include <iostream>
 #include <fstream>
 #include <chrono>
@@ -20,81 +22,75 @@ Mist the terminal assistant.
 #include "make_files.cpp"
 
 
-using namespace std::this_thread;
-using namespace std::chrono_literals;
-using std::chrono::system_clock;
-
-void time_delay();
-
-
 int main(){
-    greetings();
-    time_delay();
+    std::string username;
+    greetings(); // grabs function from greetings.cpp
+    sleep(2); // time delay (2 seconds)
     int choice;
-    list();
-    std::cin >> choice;
+    list(); // calls list function from tasks.cpp
+    std::cin >> choice; // user input
+    printf("enter your username: \n");
+    std::cin >> username; // gets your username then converts it to a string.
 
     switch(choice){
         case 1:
-            jokes();
+            jokes(); // calls jokes function from jokes.cpp
             break;
         case 2:
-            system("neofetch --stdout");
+            system("neofetch --stdout"); // activates system command neofetch
             break;
         case 3:
-            system("date");
+            system("date"); // activates date system command
             break;
         case 4:
-            make_me();
+            make_me(); // calls make_me function from make_files.cpp
             break;
         case 5:
-            system("code");
+            system("code"); // activates code system command(if vscode was installed)
             break;
         case 6:
-            system("firefox");
+            system("firefox"); // runs firefox
             break;
         case 7:
-            system("xdg-open https://github.com/");
+            system("xdg-open https://github.com/"); // opens github
             break;
         case 8:
-            std::cout << "goodbye xero" << std::endl;
-            time_delay();
-            system("shutdown now");
+            std::cout << "goodbye" << username << std::endl;
+            sleep(3); // sleeps for 3 seconds
+            system("shutdown now"); // sutsdown system
             break;
         case 9:
-            calculator();
+            calculator(); // activates calculator function from calculator.cpp
             break;
         case 10:
-            games();
+            games(); // activates game function from games.cpp
             break;
         case 11:
-            system("sudo apt update && apt upgrade");
+            system("sudo apt update && apt upgrade"); // activates update protocol
             break;
         case 12:
-            file_hunter();
+            file_hunter(); // activates file_hunter function from track_down.cpp
             break;
         case 13:
-            int extension_name;
-            printf("enter extension name: \n");
-            std::cin >> extension_name;
-            system ("sudo apt install " + extension_name);
+            int downloadble_content_name; 
+            printf("enter downloadble content name: \n");
+            std::cin >> downloadble_content_name; // takes user input
+            system ("sudo apt install " + downloadble_content_name); // calls system install command along with user input
             break;
         case 14:
-            system("sudo -s");
+            system("sudo -s"); // activates super user
             break;
         case 15:
-            system("exit");
-            break;
+            break; // breaks out
     }
+
+    // lets you repeat the switch commands or exit.
     printf("need to complete any other tasks? y/n\n");
     char choices;
     std::cin >> choices;
     if (choices == 'y' || choices == 'Y'){
         main();
     } else{exit(0);}
-}
-
-void time_delay(){
-    sleep_for(2s);
-    sleep_until(system_clock::now() + 2s);
+    
+    return 0;
 }
