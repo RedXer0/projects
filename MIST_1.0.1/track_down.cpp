79d82675a907a2bc9d1bac36a15dfcdd958e0e31
Red Xer0 <165036246+RedXer0@users.noreply.github.com>
2024-04-10 12:55:38 -0500

    Update track_down.cpp

--- MIST_1.0.1/track_down.cpp
+++ MIST_1.0.1/track_down.cpp
@@ -4,11 +4,14 @@
 void accurate_file_search();
 void search_by_extension();
 void search_type();
+
+// main function for mist.cpp
 int file_hunter(){
     int choice;
-    search_type();
-    std::cin >> choice;
+    search_type(); // calls search_type function
+    std::cin >> choice; // user input
 
+    // loops through the switch loop based on which number is inputted
     switch(choice){
         case 1:
             accurate_file_search();
@@ -20,56 +23,66 @@ int file_hunter(){
             exit(0);
             break;
         default:
-            std::cout << "Invalid choice" << std::endl;
+            std::cout << "Invalid choice" << std::endl; // basically exits the program.
             break;
     }
     return 0;
 }
 
+// search_type cunction.
 void search_type(){
-    int color = 31;
+    int color = 31; // color
+
+    // search list array
     std::string search_types[] = {
     "1. Accurate File Search.",
     "2. By Extension File Search. ",
     "3. Exit"
     };
 
-    for (int i=0; i<3; i++){
+    for (int i=0; i<3; i++){ // loops through array until the entire array is printed out.
         std::cout << "\033[" << color << "m" << search_types[i] << "\033" << std::endl;
-        printf("||--------------------------\n");
+        printf("||--------------------------\n"); // seperator lol
     }
 }
 
+// accurate file search function.
+// this allows you to find any file within any directory by just the name and file extension.
 void accurate_file_search(){
     std::string file_extension;
     std::string file_name;
     int user_input;
     printf(" insert file extension: \n");
-    std::cin >> file_extension;
-    printf("does the file have a name: 0=no =yes\n");
-    std::cin >> user_input;
-    switch(user_input){
+    std::cin >> file_extension; // file extension user input
+    printf("does the file have a name: 0=no 1=yes\n");
+    std::cin >> user_input; // response protocol
+    switch(user_input){ // switches through loop depending on which number you insert
         case 0:
-            break;
+            break; // breaks if you insert 0
         case 1:
             printf("enter file name: \n");
-            std::cin >> file_name;
+            std::cin >> file_name; // if you picked 1 you insert the name of the file
             break;
         default:
-        std::cout << "Invalid choice" << std::endl;
+        std::cout << "Invalid choice" << std::endl; // otherwise it does nothing
         break;
     }
 
+    // system file search command DONT CHANGE THIS OK THIS ALLOWS YOU TO SEARCH FOR FILES!!!
     std::string command = "find /  2>/dev/null ! -readable -prune -o -name \'" + file_name + "." + file_extension + "\' -print -quit";
-    system(command.c_str());
+    system(command.c_str()); // prints the string data into the system then runs it
     
 }
 
+
+// this searches for all files with the given extension type.
 void search_by_extension(){
     std::string file_extension;
     printf("insert file extension: \n");
-    std::cin >> file_extension;
+    std::cin >> file_extension; // user input.
+
+    // DO NOT CHANGE!! this is the string file that allows you to search for files by extension.
     std::string command = "find / 2>/dev/null ! -readable -prune -o -name \"*." + file_extension + "\"" + " -print";
 
-    system(command.c_str());
+    system(command.c_str()); // inputs the command string into the system.
 }
